b25e587e5c5a1a120e552cf833970b5ccc483d68
Konrad Rolof <konrad.rolof@gmail.com>
2017-03-11 13:59:25 +0100

    added content to main and some new style files

--- demo/index.php
+++ demo/index.php
@@ -18,7 +18,14 @@ require_once __DIR__ . '/inc/_bootstrap.inc.php';
 
         </section>
         <main class="site-main medium-8 large-6 columns">
-
+            <div class="box">
+                <h2>What is modular click dummy?</h2>
+                <p>Building click dummies as a prototype for enormous frameworks like Symfony or Typo3 is often a lot of
+                    copy and past of markup. It is not just wasted time to copy your code. The big trouble comes with
+                    working in a team or if you must make changes.<br>
+                    So it's better to write the markup of a module just one time an include it at the points where it's
+                    needed. This little system should help you the build click dummies easier. </p>
+            </div>
         </main>
         <section class="site-right-bar medium-4 large-3 columns">
 
